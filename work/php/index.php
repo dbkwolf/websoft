2370d3c8f16e9aef3616eecbe4610b357e9b36c4
Delay Wolfsteller <33721623+dbkwolf@users.noreply.github.com>
2020-02-18 00:52:17 +0100

    add link to a5

--- work/php/index.php
+++ work/php/index.php
@@ -1,1 +1,18 @@
-this is index php
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Delay -</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body class="blue-grey darken-6">
+    <div class="valign-wrapper">
+      <div class="container">
+        <h5 class="white-text">Content TBA</h5>
+        <p class="white-text">Just a placeholder at the moment.</p>
+        </div>
+      </div>
+  </body>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+</html>
